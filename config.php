effb5525242ca62252c0c2ff47cc8518240b16a8
Dries Swinnen <swinnendries@gmail.com>
2020-08-19 10:18:35 +0200

    refactor db creds to config.php file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    // Define mysql server settings
+    $serverName = "localhost";
+    $username = "root";
+    $password = "";
+    $dbName = "employees";
+
+    // Create a new mysqli connection. Remember to enable this in php.ini !!
+    $conn = new mysqli($serverName, $username, $password, $dbName);
--- index.php
+++ index.php
@@ -1,27 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-     <title>Awsome PXL CI/CD App!</title>
+     <title>Awsome PXL DevOps App!</title>
      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
      <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>
-    <h1>PXL - CI/CD Demo application</h1><hr/>
+    <h1>PXL - DevOps Demo application</h1><hr/>
         <?php
         // Use 'composer install' to get dependencies
         // Remember to run tests using PHPUnit: 'vendor/bin/phpunit tests'
 
         // composer autoload. Might require 'composer dump-autoload' to work.
         require('vendor/autoload.php');
-
-        // Define mysql server settings
-        $serverName = "localhost";
-        $username = "root";
-        $password = "ventieldopje24";
-        $dbName = "employees";
-
-        // Create a new mysqli connection. Remember to enable this in php.ini !!
-        $conn = new mysqli($serverName, $username, $password, $dbName);
+        require_once('config.php');
         // Build sql query and get results
         $sql = "SELECT emp_no, first_name, last_name FROM employees";
         $r = $conn->query($sql);
